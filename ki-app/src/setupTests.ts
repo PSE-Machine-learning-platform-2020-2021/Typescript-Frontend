d38b3000a822ff56468987303eac2150ba8e440d
Morridin <paulza@gmx.de>
2021-01-26 18:03:18 +0100

    Added some Test contents to setupTests.ts

--- ki-app/src/setupTests.ts
+++ ki-app/src/setupTests.ts
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import React from 'react'
+
+class Test extends React.Component {
+    render() {
+        return <p>Test paragraph.</p>;
+    };
+
+    and(a: boolean, b: boolean): boolean {
+        return (a && b);
+    };
+}
