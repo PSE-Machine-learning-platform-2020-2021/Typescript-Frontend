a586802e0ece608c33cfd95d84ba0d3dcfafd5dc
Morridin <paulza@gmx.de>
2021-01-28 12:57:06 +0100

    model.Extraction
    Created TypeScript file
    Filled in whole enum
    Comments still missing

---
+++ ki-app/src/model/Extraction.tsx
@@ -0,0 +1,13 @@
+export enum Extraction {
+    MIN,
+    MAX,
+    VARIANCE,
+    ENERGY,
+    MEAN,
+    AUTOREGRESSIVE,
+    IQR,
+    PEARSON_CORRELATION,
+    SKEWNESS,
+    KURTOSIS,
+    FOURIER_TRANSFORM
+}
