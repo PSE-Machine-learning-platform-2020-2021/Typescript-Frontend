0d1d8ce5659417020c30a0646e23b1d9e4ad99da
Morridin <paulza@gmx.de>
2021-01-28 12:50:53 +0100

    model.AIBuilder
    Filled file with class skeleton and method documentation comments.

---
+++ ki-app/src/model/AIBuilder.tsx
@@ -0,0 +1,37 @@
+/**
+ * Diese Klasse verwaltet die Interaktion mit dem Python-Backend und die zugehörige Kommunikation.
+ * Sie ist verantwortlich für den Bau von KI-Modellen und für die Ausführung derselben.
+ */
+export class AIBuilder {
+    private modelId: number
+    private modelData: number
+
+    /**
+     * Erstellt den AIBuilder und übergibt ihm die ID des zu bearbeitenden Modells.
+     * @param modelId Die ID des zu erstellenden oder zu verwendenden Modells
+     */
+    constructor(modelId: number) {
+        this.modelId = modelId
+    }
+
+    /**
+     * Ruft die Python-Schnitstelle auf dem Server auf, um ein KI-Modell zu erstellen und zu trainieren.
+     * @param modelData Sämtliche relevanten Daten, um das gewünschte KI-Modell erstellen zu können.
+     */
+    applyModel(modelData: object): void {
+
+    }
+
+    /**
+     * 
+     * @param dataSetId Die ID des Datensatzes, den das KI-Modell klassifizieren soll.
+     * @param callBack Eine Rückmelde-Funktion des "aufrufenden" Controllers, mit der das Ergebnis der Klassifizierung an den Controller zurückgegeben wird.
+     */
+    classify(dataSetId: number, callBack: <T = unknown, R = unknown>(prediction: T) => R): void {
+
+    }
+
+    private setUpServerConnection(): unknown {
+        return null
+    }
+}
