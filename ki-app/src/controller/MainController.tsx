e16e62260717a327a937db1c02c6b200a8d92e5a
Darius Seiter <Darius.Seiter@outlook.com>
2021-01-30 19:05:51 +0100

    Implementierung des MainController

--- ki-app/src/controller/MainController.tsx
+++ ki-app/src/controller/MainController.tsx
@@ -1,23 +1,60 @@
-import { MainControllerInterface } from "./MainControllerInterface";
+import { PageController } from "./PageController";
+import { StartController } from "./StartController";
+import {RefferingController} from "./ReferringController";
 export class MainController {
-    static getInstance() {
-        return self
-    }
 
+    private static facade : view.Facade;
+
+    private static currentPageController : PageController;
+
+    /**
+    * Prüft ob das Gerät auf das Internet zugreifen kann.
+    * @returns Gibt true zurück falls eine Internetverbindung besteht, sonst wird false zurück gegeben.
+    */
     static checkConnection() {
-        return false
+        return (window.navigator.onLine);
     }
 
+    /**
+    * Prüft ob der Benutzer angemeldet ist.
+    * @returns Gibt true zurück falls der Benutzer angemeldet ist, sonst wird false zurück gegeben.
+    */
     static checkLoginStatus() {
-        return false
+        return (this.facade.checkLogin());
     }
-    static getSession() {
-        return false
+
+    // static getSession() {
+    //    return false
+    //}
+
+    /**
+    * Setzt den momentanen Seitenverwalter neu
+    * @param destinationPageController Der neue Seitenverwalter
+    */
+    static changeTo(destinationPageController: PageController){
+        this.currentPageController = destinationPageController;
     }
-    static changeTo(destinationPage: view.Page){
-        
+
+    /**
+    * Andwendungstart für einen Desktop
+    */
+    static startDesktop() {
+        let refferingController: RefferingController  = new RefferingController()
+        this.changeTo(refferingController)
     }
+
+    /**
+    * Andwendungstart für ein Smartphone
+    */
+    static startSmartphone() {
+        let startController: StartController  = new StartController()
+        this.changeTo(startController)
+    }
+
+    /**
+    * @returns Gibt die Fassade zurück
+    */
     static getFacade() {
-        return facade
+        return this.facade;
     }
 }
