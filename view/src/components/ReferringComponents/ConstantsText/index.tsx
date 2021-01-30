e6e6cbb5c5294e7c52e787aa579bca224d698bec
wtc <wtc19990629@gmail.com>
2021-01-30 16:11:10 +0100

    components-init

---
+++ view/src/components/ReferringComponents/ConstantsText/index.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ConstantsText extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
