e6eecca1fde81b299840966af424f1e4d1b1af47
wtc <wtc19990629@gmail.com>
2021-01-30 23:32:08 +0100

    link+qrimage

--- view/src/components/ReferringComponents/LinkText/index.tsx
+++ view/src/components/ReferringComponents/LinkText/index.tsx
@@ -3,8 +3,14 @@ import React, { Component } from 'react'
 export default class LinkText extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="linktext">
+                <div className="linktext1">
+                <p>QR-Code scannen oder Link folgen, um neues Projekt zu beginnen:</p> 
+                </div>
+                <div className="linktext2">
+                {/*wait backend to change*/}
+                <a href="http://www.google.de">http://www.google.de</a> 
+                </div>
             </div>
         )
     }
--- view/src/pages/ReferringPage/index.tsx
+++ view/src/pages/ReferringPage/index.tsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import ConstantsText from '../../components/ReferringComponents/ConstantsText'
+import LinkText from '../../components/ReferringComponents/LinkText'
+import QRImage from '../../components/ReferringComponents/QRImage'
 
 export default class ReferringPage extends Component {
     render() {
         return (
             <div>
                 <ConstantsText/>
+                <QRImage/>
+                <LinkText/>
             </div>
         )
     }
