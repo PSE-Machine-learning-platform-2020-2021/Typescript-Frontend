3f12f60e0b5b03adce80e08c72969b1e18f857ab
wtc <wtc19990629@gmail.com>
2021-01-31 16:05:29 +0100

    loginButton

--- view/src/components/ReferringComponents/LoginButton/index.jsx
+++ view/src/components/ReferringComponents/LoginButton/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react'
+import React, { Component } from 'react'
 import NewWindow from "react-new-window";
 
 export default class index extends Component {
