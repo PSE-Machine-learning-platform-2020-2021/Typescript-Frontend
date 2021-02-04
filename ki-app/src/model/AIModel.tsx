3863c1fee83a0a256df4f3389f99c51abcf5e550
Timo Merkel <timi300399@outlook.de>
2021-02-04 10:22:04 +0100

    AIModel Kommentare angepasst

--- ki-app/src/model/AIModel.tsx
+++ ki-app/src/model/AIModel.tsx
@@ -1,13 +1,20 @@
-//Diese Klasse beinhaltet nur die ID des AI Models, die auf das Model in der Datenbank verweist.
+/**
+ * Diese Klasse beinhaltet nur die ID des AI Models, die auf das Model in der Datenbank verweist.
+ */
 class AIModel {
     private id:number //Die AI Model ID.
 
-    //Die übergebene ID muss mit der AIModel ID in der Datenbank übereinstimmen.
+    /**
+     * Die übergebene ID muss mit der AIModel ID in der Datenbank übereinstimmen.
+     * @param id AIModel ID
+     */
     constructor(id:number) {
         this.id = id;
     }
 
-    //Gibt die AIModel ID zurück.
+    /**
+     * Gibt die AIModel ID zurück.
+     */
     getID():number {
         return this.id;
     }
