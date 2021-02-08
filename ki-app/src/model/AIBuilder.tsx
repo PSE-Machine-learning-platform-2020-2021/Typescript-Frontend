9f799da96a693078653eee9c6541aabe2f51ee33
Morridin <paulza@gmx.de>
2021-02-08 18:07:37 +0100

    Klasse AIBuilder vollstÃndig implementiert
    Alle Ã¶ffentlichen Funktionen erzeugt.
    
    Kleine EntwurfsÃ¤nderung: Der Parametertyp der Callbackfunktion muss
    jetzt vom Typ string|object sein; es MUSS beides akzeptiert werden.
    Dies dient als RÃckfallebene, falls eine Anfrage aus unbekannten GrÃnden
    kein valides JSON zurÃckliefert.
    
    TODO: URLs der Dateien spÃ¤ter mal anpassen ...

--- ki-app/src/model/AIBuilder.tsx
+++ ki-app/src/model/AIBuilder.tsx
@@ -1,10 +1,15 @@
+import { findAllByPlaceholderText } from "@testing-library/react"
+import { Facade, FacadeInterface } from "./Facade"
+
 /**
  * Diese Klasse verwaltet die Interaktion mit dem Python-Backend und die zugehörige Kommunikation.
  * Sie ist verantwortlich für den Bau von KI-Modellen und für die Ausführung derselben.
  */
 export class AIBuilder {
     private modelId: number
-    private modelData: number
+    private static readonly url: string = "python/"
+    private static readonly buildModel: string = "buildModel.php"
+    private static readonly classify: string = "classify.php"
 
     /**
      * Erstellt den AIBuilder und übergibt ihm die ID des zu bearbeitenden Modells.
@@ -19,7 +24,7 @@ export class AIBuilder {
      * @param modelData Sämtliche relevanten Daten, um das gewünschte KI-Modell erstellen zu können.
      */
     applyModel(modelData: object): void {
-
+        this.sendRequest(JSON.stringify(modelData), AIBuilder.url + AIBuilder.buildModel, console.log)
     }
 
     /**
@@ -28,10 +33,45 @@ export class AIBuilder {
      * @param callBack Eine Rückmelde-Funktion des "aufrufenden" Controllers, mit der das Ergebnis der Klassifizierung an den Controller zurückgegeben wird.
      */
     classify(dataSetId: number, callBack: <T = unknown, R = unknown>(prediction: T) => R): void {
-
+        let requestData: string = JSON.stringify({"classifier": this.modelId, "dataSet": dataSetId})
+        this.sendRequest(requestData, AIBuilder.url + AIBuilder.classify, callBack)
     }
 
-    private setUpServerConnection(): unknown {
-        return null
+    /**
+     * Diese private Methode fässt den Versandprozess der Subanfrage an den Anwendungsserver zusammen.
+     * @param requestData Die mit der Anfrage zu versendenden Daten. Hat gültiges JSON zu sein, außer plainText ist gesetzt.
+     * @param url Die Adresse, an die die Anfrage gestellt wird. Am besten was auf demselben Server, ansonsten bekommt der Browser nen Kasper.
+     * @param successAction Eine Funktion, die bei erfolgreicher Durchführung der Anfrage auszuführen ist.
+     * @param plainText Gibt an, dass die Daten als Plaintext zu versenden sind.
+     */
+    private sendRequest(requestData: string, url: string, successAction: <R = unknown>(param: string|object) => R, plainText: boolean = false) {
+        let xhr = new XMLHttpRequest() // XHR ist kurz für XmlHttpRequest
+        xhr.open("POST", url, true)
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    let responseJSON: object
+                    try {
+                        responseJSON = JSON.parse(xhr.responseText)
+                    }
+                    catch (e) {
+                        responseJSON = null
+                    }
+                    if(responseJSON == null) {
+                        successAction(xhr.responseText)
+                    }
+                    else {
+                        successAction(responseJSON)
+                    }
+                }
+                else {
+                    xhr.send(requestData)
+                }
+            }
+        }
+        if (!plainText) {
+            xhr.setRequestHeader("Content-Type","application/json")
+        }
+        xhr.send(requestData)
     }
 }
