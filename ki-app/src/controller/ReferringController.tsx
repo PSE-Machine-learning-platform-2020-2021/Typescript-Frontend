fbf459d0a6f970b0833ae1e90b294244b6766953
Darius Seiter <Darius.Seiter@outlook.com>
2021-02-08 16:30:01 +0100

    ReferringController implementiert und qr Generator eingefügt

--- ki-app/src/controller/ReferringController.tsx
+++ ki-app/src/controller/ReferringController.tsx
@@ -1,26 +1,101 @@
 import { PageController } from "./PageController";
+import { MainController } from "./MainController";
+import { VisualizationController } from "./VisualizationController";
+import { AIController } from "./AIController";
+import { qrcodegen } from "../qrcode/qrcodegen";
 
-export class RefferingController implements PageController{
-    currentProjekt: object
-    createQR(sessionNum: string) {
-        return string
+export class RefferingController implements PageController {
+    private page = new view.StartPage();
+    private qrc = qrcodegen.QrCode;
+    /**
+     * Konstruktor des Seitenverwalters. Registriert sich als Beobachter auf seiner Seite und setzt den start Status. 
+     */
+    constructor() {
+        this.page.attach(this);
+        this.page.setState("needMessage");
     }
-    createLink(sessionNum: string) {
-        return string
-    }
-    login(name: string, password: string) {
-        return false
-    }
-    createNewProject(projectName: string) {
 
-    }
-    loadModel(projectId: number, modelId: number, userId: number) {
-
-    }
-    setLanguage() {
-
-    }
+    /**
+     * Die Update Methode des Seitenverwalters.
+     */
     update() {
+        let state = this.page.getState();
+        switch (state.action) {
+            case "needQR":
+                break;
+            case "neenLink":
+                break;
+            case "login":
+                break;
+            case "newProjekt":
+                break;
+            case "loadModel":
+                break;
+            case "setLanguage":
+                let languageCode = this.page.getLanguageCode();
+                this.page.languageChanged(MainController.getInstance().setLanguage(languageCode));
+                break;
+            case "needMessage":
+                let ids = this.page.getIds();
+                this.page.setMessages(MainController.getInstance().getMessage(ids));
+                break;
+            default:
+                break;
+        }
+    }
 
+    login() {
+        let loginData: string[] = this.page.getLoginData();
+        let loginSucess: boolean = MainController.getInstance().getFacade().loginAdmin(loginData[0], loginData[0]);
+        if (loginSucess) {
+            let projectMeta: string[] = MainController.getInstance().getFacade().getProjectMetas();
+            this.page.setProjectMeta(projectMeta);
+            this.page.setState("loggedIn");
+        } else {
+            this.page.setState("loginFailed");
+        }
+    }
+
+    createQR() {
+        let link: string = MainController.getInstance().getFacade().getDataMinerLink();
+        let qr0 = this.qrc.encodeText(link, this.qrc.Ecc.MEDIUM);
+        let svg = qr0.toSvgString(4);
+        this.page.setQRsvg(svg);
+        this.page.setState("showQRcode");
+    }
+
+    createNewProject(projectName: string) {
+        let sucess: boolean = MainController.getInstance().getFacade().createProject(this.page.getNewProjectName());
+        if (sucess) {
+            this.page.setState("needQR");
+        } else {
+            this.page.setState("ProjectError");
+        }
+    }
+
+    loadProject() {
+        let projectId: number = this.page.getProjectID;
+        let sucess: boolean = MainController.getInstance().getFacade().loadProject(projectId);
+        if (sucess) {
+            this.page.setState("needQR");
+        } else {
+            this.page.setState("ProjectError");
+        }
+    }
+
+    loadModel() {
+        let projectId: number = this.page.getProjectID;
+        let sucess: boolean = MainController.getInstance().getFacade().loadProject(projectId);
+        if (sucess) {
+            let aiController: AIController = new AIController();
+            MainController.getInstance().changeTo(aiController);
+        } else {
+            this.page.setState("ProjectError");
+        }
+    }
+
+    changeToVisualization() {
+        let visualizationController: VisualizationController = new VisualizationController();
+        MainController.getInstance().changeTo(visualizationController);
     }
 }
