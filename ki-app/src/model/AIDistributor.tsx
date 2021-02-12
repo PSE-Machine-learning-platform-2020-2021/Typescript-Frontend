763377dfd5e85bd6884455b12e0a61d56b8e69b7
Morridin <paulza@gmx.de>
2021-02-12 13:45:08 +0100

    Implementierung AIDistributor abgeschlossen.
    Eine unnÃ¶tige, private Methode entfernt.

--- ki-app/src/model/AIDistributor.tsx
+++ ki-app/src/model/AIDistributor.tsx
@@ -5,6 +5,7 @@ import {DeliveryFormat} from './DeliveryFormat'
  * Diese Klasse verwaltet die Auslieferungsformalitäten für trainierte KI-Modelle.
  */
 export class AIDistributor {
+    private static readonly url: string = "python/deliverance.php"
     private format: DeliveryFormat
     private id: number
 
@@ -24,22 +25,39 @@ export class AIDistributor {
      * später als Web-Anwendung auszuführen.
      */
     getAIModel(): object  {
-        return {}
+        let xhr = new XMLHttpRequest() // XHR ist kurz für XmlHttpRequest
+        let data = {}
+        xhr.open("POST", AIDistributor.url, true)
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4) {
+                if(xhr.status == 200) {
+                    data = JSON.parse(xhr.responseText)
+                }
+                else {
+                    console.log("Connection to server failed. Please try again.")
+                }
+            }
+        }
+        xhr.setRequestHeader("Content-Type", "application/json")
+        xhr.send(JSON.stringify({"id": this.id, "format": this.format}))
+
+        switch (this.format) {
+            case DeliveryFormat.WEB_APP:
+                return data
+            case DeliveryFormat.EXE:
+                location.href = data["exe"]
+            default:
+                throw new Error("Illegal delivery format.")
+        }
     }
 
     /**
      * Diese Methode wird verwendet, um aus einem aus der Datenbank geladenen 
      * KI-Modell (Scaler und Classifier) eine gebrauchsfertige Anwendung zu erstellen.
+     * 
+     * Diese Methode ist sinnlos, da eine Datei auf dem Server liegen muss, um heruntergeladen werden zu können.
      */
     private buildExecutable(): boolean {
         throw new Error("Not Implemented")
-    }
-
-    /**
-     * Diese Methode erzeugt einen Zugang zur Datenbank.
-     * Ggf. ist eine Umbenennung sinnvoll, wenn diese Methode auch das KI-Modell holen soll.
-     */
-    private setUpDataBaseConnection(): void {
-
     }
 }
