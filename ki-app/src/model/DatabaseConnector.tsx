916552391c6f5394a654d7ff1236522d2ccdeb70
Timo Merkel <timi300399@outlook.de>
2021-02-16 18:49:01 +0100

    Database bearbeiten

--- ki-app/src/model/DatabaseConnector.tsx
+++ ki-app/src/model/DatabaseConnector.tsx
@@ -21,8 +21,8 @@ export class DatabaseConnector {
    * @param languageCode Sprachcode
    * @returns die Sprache
    */
-  async loadLanguage(languageCode: string): Promise<string[]> {
-    const data: string = await this.sendRequest("loadLanguage", JSON.stringify(languageCode));
+  async loadLanguage(requestData: { languageCode: string; }): Promise<string[]> {
+    const data: string = await this.sendRequest("loadLanguage", requestData);
     try {
       const result: string[] = JSON.parse(data);
       return result;
@@ -41,7 +41,7 @@ export class DatabaseConnector {
    * @returns ProjektID und die SessionID, falls das Projekt nicht erstellt werden konnte beides -1
    */
   async createProject(requestData: { userID: number, adminEmail: string, projectName: string; }): Promise<{ projectID: number, sessionID: number; }> {
-    const data: string = await this.sendRequest("createProject", JSON.stringify(requestData));
+    const data: string = await this.sendRequest("createProject", requestData);
     try {
       const result: { projectID: number, sessionID: number; } = JSON.parse(data);
       return result;
@@ -62,7 +62,7 @@ export class DatabaseConnector {
    *          bei fehler, -1
    */
   async createDataSet(requestData: { sessionID: number, projectID: number, userID: number, dataSetName: string, dataRow: { sensorID: number, datarowName?: string; }[]; }): Promise<number> {
-    const data: string = await this.sendRequest("createDataSet", JSON.stringify(requestData));
+    const data: string = await this.sendRequest("createDataSet", requestData);
     try {
       const result: number = JSON.parse(data);
       return result;
@@ -81,7 +81,7 @@ export class DatabaseConnector {
    * @param datapoint 
    */
   async sendDataPoint(requestData: { sessionID: number, userID: number, dataSetID: number, dataRowID: number, datapoint: { value: number, relativeTime: number; }; }): Promise<boolean> {
-    const data: string = await this.sendRequest("sendDataPoint", JSON.stringify(requestData));
+    const data: string = await this.sendRequest("sendDataPoint", requestData);
     try {
       const result: boolean = JSON.parse(data);
       return result;
@@ -109,7 +109,7 @@ export class DatabaseConnector {
       label: { name: string, labelID: number, start: number, end: number; }[];
     }[];
   }> {
-    const data: string = await this.sendRequest("loadProject", JSON.stringify(requestData));
+    const data: string = await this.sendRequest("loadProject", requestData);
     try {
       const result: {
         projectID: number, sessionID: number, projectName: string, aiModelID: number[],
@@ -139,7 +139,7 @@ export class DatabaseConnector {
    * @param adminEmail zur Sicherheit, muss zur UserID übereinstimmen
    */
   async getProjectMetas(requestData: { userID: number, adminEmail: string; }): Promise<{ projectID: number, projectName: string, AIModelID: number[]; }[]> {
-    const data: string = await this.sendRequest("getProjectMetas", JSON.stringify(requestData));
+    const data: string = await this.sendRequest("getProjectMetas", requestData);
     try {
       const result: { projectID: number, projectName: string, AIModelID: number[]; }[] = JSON.parse(data);
       return result;
@@ -158,7 +158,7 @@ export class DatabaseConnector {
    * @returns ob der Datenset erfolgreich gelöscht wurde
    */
   async deleteDataSet(requestData: { userID: number, adminEmail: string, projectID: number, dataSetID: number; }): Promise<boolean> {
-    const data: string = await this.sendRequest("deleteDataSet", JSON.stringify(requestData));
+    const data: string = await this.sendRequest("deleteDataSet", requestData);
     try {
       const result: boolean = JSON.parse(data);
       return result;
@@ -178,7 +178,7 @@ export class DatabaseConnector {
    * @returns Gibt die Daten zurück, als Fehler werden alle IDs auf -1 gesetzt
    */
   async registerAdmin(requestData: { adminName: string, adminEmail: string, password: string, device: { deviceID?: number, deviceName: string, deviceType: string, firmware: string, generation: string, MACADRESS: string, sensorInformation: { sensorTypeID: number, sensorName: string, sensorUniqueID: number; }[]; }; }): Promise<{ adminID: number, device: { deviceID: number, sensorID: number[]; }; }> {
-    const data: string = await this.sendRequest("registerAdmin", JSON.stringify(requestData));
+    const data: string = await this.sendRequest("registerAdmin", requestData);
     try {
       const result: { adminID: number, device: { deviceID: number, sensorID: number[]; }; } = JSON.parse(data);
       return result;
@@ -196,7 +196,7 @@ export class DatabaseConnector {
    * @returns Gibt die Daten zurück, als Fehler werden alle IDs auf -1 gesetzt
    */
   async registerDataminer(requestData: { dataminerName: string, sessionID: number, device: { deviceID?: number, deviceName: string, deviceType: string, firmware: string, generation: string, MACADRESS: string, sensorInformation: { sensorTypeID: number, sensorName: string, sensorUniqueID: number; }[]; }; }): Promise<{ dataminerID: number, device: { deviceID: number, sensorID: number[]; }, project: { projectID: number, projectName: string, sessionID: number; }; }> {
-    const data: string = await this.sendRequest("registerDataminer", JSON.stringify(requestData));
+    const data: string = await this.sendRequest("registerDataminer", requestData);
     try {
       const result: { dataminerID: number, device: { deviceID: number, sensorID: number[]; }, project: { projectID: number, projectName: string, sessionID: number; }; } = JSON.parse(data);
       return result;
@@ -216,7 +216,7 @@ export class DatabaseConnector {
    * @returns Gibt die Daten zurück, als Fehler werden alle IDs auf -1 gesetzt
    */
   async registerAIModelUser(requestData: { aiModelUserName: string, modelID: number, device: { deviceID?: number, deviceName: string, deviceType: string, firmware: string, generation: string, MACADRESS: string, sensorInformation: { sensorTypeID: number, sensorName: string, sensorUniqueID: number; }[]; }; }): Promise<{ aiModelUserID: number, device: { deviceID: number, sensorID: number[]; }, project: { projectID: number, projectName: string, sessionID: -1; }; }> {
-    const data: string = await this.sendRequest("registerAIModelUser", JSON.stringify(requestData));
+    const data: string = await this.sendRequest("registerAIModelUser", requestData);
     try {
       const result: { aiModelUserID: number, device: { deviceID: number, sensorID: number[]; }, project: { projectID: number, projectName: string, sessionID: -1; }; } = JSON.parse(data);
       return result;
@@ -232,7 +232,7 @@ export class DatabaseConnector {
    * @param password 
    */
   async loginAdmin(requestData: { adminEmail: string, password: string; }): Promise<{ admin: { adminID: number, deviceID: number, adminName: string, email: string, device: { deviceID?: number, deviceName: string, deviceType: string, firmware: string, generation: string, MACADRESS: string, sensorInformation: { sensorTypeID: number, sensorName: string, sensorUniqueID: number; }[]; }; }; }> {
-    const data: string = await this.sendRequest("loginAdmin", JSON.stringify(requestData));
+    const data: string = await this.sendRequest("loginAdmin", requestData);
     try {
       const result: { admin: { adminID: number, deviceID: number, adminName: string, email: string, device: { deviceID?: number, deviceName: string, deviceType: string, firmware: string, generation: string, MACADRESS: string, sensorInformation: { sensorTypeID: number, sensorName: string, sensorUniqueID: number; }[]; }; }; } = JSON.parse(data);
       return result;
@@ -257,7 +257,7 @@ export class DatabaseConnector {
    * @returns labelID
    */
   async createLabel(requestData: { sessionID: number, userID: number, datasetID: number, label: { span: { start: number, end: number; }, labelName: string; }; }): Promise<number> {
-    const data: string = await this.sendRequest("createLabel", JSON.stringify(requestData));
+    const data: string = await this.sendRequest("createLabel", requestData);
     try {
       const result: number = JSON.parse(data);
       return result;
@@ -275,7 +275,7 @@ export class DatabaseConnector {
    * @param label 
    */
   async setLabel(requestData: { sessionID: number, userID: number, datasetID: number, label: { labelID: number, span: { start?: number, end?: number; }, labelName?: string; }; }): Promise<boolean> {
-    const data: string = await this.sendRequest("setLabel", JSON.stringify(requestData));
+    const data: string = await this.sendRequest("setLabel", requestData);
     try {
       const result: boolean = JSON.parse(data);
       return result;
@@ -293,7 +293,7 @@ export class DatabaseConnector {
    * @param labelID 
    */
   async deleteLabel(requestData: { sessionID: number, userID: number, datasetID: number, labelID: number; }): Promise<boolean> {
-    const data: string = await this.sendRequest("deleteLabel", JSON.stringify(requestData));
+    const data: string = await this.sendRequest("deleteLabel", requestData);
     try {
       const result: boolean = JSON.parse(data);
       return result;
@@ -304,9 +304,10 @@ export class DatabaseConnector {
 
   }
 
-  private async sendRequest(action: string, requestDataJSON?: string): Promise<string> {
+  private async sendRequest(action: string, requestData?: object): Promise<string> {
     const headers = { 'Content-Type': 'application/json' };
-    const response = await fetch(DatabaseConnector.databasePHPURL + "?action=" + action, { headers, body: undefined }); //TODO body: requestDataJSON wirft Fehler
+    const response = await fetch(DatabaseConnector.databasePHPURL + "?action=" + action, { headers, body: JSON.stringify(requestData) }); //TODO body: requestDataJSON wirft Fehler
+    const data = response.json();
     return data;
   }
 }
