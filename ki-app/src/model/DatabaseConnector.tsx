ec7df78e8b4672ed09c5af3541cd1f3aa19210b3
Timo Merkel <timi300399@outlook.de>
2021-02-17 18:34:49 +0100

    modify package for server and fix Database error

--- ki-app/src/model/DatabaseConnector.tsx
+++ ki-app/src/model/DatabaseConnector.tsx
@@ -6,9 +6,9 @@ export class DatabaseConnector {
    * Gibt Name und Code jeder verfügbaren Sprache zurück
    */
   async getLanguageMetas(): Promise<{ languageCode: number, languageName: string; }[]> {
-    const data: string = await this.sendRequest("get_language_metas");
+    const response = await this.sendRequest("get_language_metas");
     try {
-      const result: { languageCode: number, languageName: string; }[] = JSON.parse(data);
+      const result: { languageCode: number, languageName: string; }[] = JSON.parse(response.json());
       console.log("LanguageCode " + result[0].languageCode + " und Name: " + result[0].languageName); //TEST
       return result;
     } catch (e) {
@@ -24,9 +24,9 @@ export class DatabaseConnector {
    */
   async loadLanguage(requestData: { languageCode: string; }): Promise<string[]> {
     this.getLanguageMetas();   //////////////////////////////////////////////////////////////////////////////TEST
-    const data: string = await this.sendRequest("loadLanguage", requestData);
+    const response = await this.sendRequest("loadLanguage", requestData);
     try {
-      const result: string[] = JSON.parse(data);
+      const result: string[] = JSON.parse(response.json());
       return result;
     } catch (e) {
       console.log(e);
@@ -43,9 +43,9 @@ export class DatabaseConnector {
    * @returns ProjektID und die SessionID, falls das Projekt nicht erstellt werden konnte beides -1
    */
   async createProject(requestData: { userID: number, adminEmail: string, projectName: string; }): Promise<{ projectID: number, sessionID: number; }> {
-    const data: string = await this.sendRequest("createProject", requestData);
+    const response = await this.sendRequest("createProject", requestData);
     try {
-      const result: { projectID: number, sessionID: number; } = JSON.parse(data);
+      const result: { projectID: number, sessionID: number; } = JSON.parse(response.json());
       return result;
     } catch (e) {
       console.log(e);
@@ -64,9 +64,9 @@ export class DatabaseConnector {
    *          bei fehler, -1
    */
   async createDataSet(requestData: { sessionID: number, projectID: number, userID: number, dataSetName: string, dataRow: { sensorID: number, datarowName?: string; }[]; }): Promise<number> {
-    const data: string = await this.sendRequest("createDataSet", requestData);
+    const response = await this.sendRequest("createDataSet", requestData);
     try {
-      const result: number = JSON.parse(data);
+      const result: number = JSON.parse(response.json());
       return result;
     } catch (e) {
       console.log(e);
@@ -83,9 +83,9 @@ export class DatabaseConnector {
    * @param datapoint 
    */
   async sendDataPoint(requestData: { sessionID: number, userID: number, dataSetID: number, dataRowID: number, datapoint: { value: number, relativeTime: number; }; }): Promise<boolean> {
-    const data: string = await this.sendRequest("sendDataPoint", requestData);
+    const response = await this.sendRequest("sendDataPoint", requestData);
     try {
-      const result: boolean = JSON.parse(data);
+      const result: boolean = JSON.parse(response.json());
       return result;
     } catch (e) {
       console.log(e);
@@ -111,7 +111,7 @@ export class DatabaseConnector {
       label: { name: string, labelID: number, start: number, end: number; }[];
     }[];
   }> {
-    const data: string = await this.sendRequest("loadProject", requestData);
+    const response = await this.sendRequest("loadProject", requestData);
     try {
       const result: {
         projectID: number, sessionID: number, projectName: string, aiModelID: number[],
@@ -123,7 +123,7 @@ export class DatabaseConnector {
           }[],
           label: { name: string, labelID: number, start: number, end: number; }[];
         }[];
-      } = JSON.parse(data);
+      } = JSON.parse(response.json());
       return result;
     } catch (e) {
       console.log(e);
@@ -141,9 +141,9 @@ export class DatabaseConnector {
    * @param adminEmail zur Sicherheit, muss zur UserID übereinstimmen
    */
   async getProjectMetas(requestData: { userID: number, adminEmail: string; }): Promise<{ projectID: number, projectName: string, AIModelID: number[]; }[]> {
-    const data: string = await this.sendRequest("getProjectMetas", requestData);
+    const response = await this.sendRequest("getProjectMetas", requestData);
     try {
-      const result: { projectID: number, projectName: string, AIModelID: number[]; }[] = JSON.parse(data);
+      const result: { projectID: number, projectName: string, AIModelID: number[]; }[] = JSON.parse(response.json());
       return result;
     } catch (e) {
       console.log(e);
@@ -160,9 +160,9 @@ export class DatabaseConnector {
    * @returns ob der Datenset erfolgreich gelöscht wurde
    */
   async deleteDataSet(requestData: { userID: number, adminEmail: string, projectID: number, dataSetID: number; }): Promise<boolean> {
-    const data: string = await this.sendRequest("deleteDataSet", requestData);
+    const response = await this.sendRequest("deleteDataSet", requestData);
     try {
-      const result: boolean = JSON.parse(data);
+      const result: boolean = JSON.parse(response.json());
       return result;
     } catch (e) {
       console.log(e);
@@ -180,9 +180,9 @@ export class DatabaseConnector {
    * @returns Gibt die Daten zurück, als Fehler werden alle IDs auf -1 gesetzt
    */
   async registerAdmin(requestData: { adminName: string, adminEmail: string, password: string, device: { deviceID?: number, deviceName: string, deviceType: string, firmware: string, generation: string, MACADRESS: string, sensorInformation: { sensorTypeID: number, sensorName: string, sensorUniqueID: number; }[]; }; }): Promise<{ adminID: number, device: { deviceID: number, sensorID: number[]; }; }> {
-    const data: string = await this.sendRequest("registerAdmin", requestData);
+    const response = await this.sendRequest("registerAdmin", requestData);
     try {
-      const result: { adminID: number, device: { deviceID: number, sensorID: number[]; }; } = JSON.parse(data);
+      const result: { adminID: number, device: { deviceID: number, sensorID: number[]; }; } = JSON.parse(response.json());
       return result;
     } catch (e) {
       console.log(e);
@@ -198,9 +198,9 @@ export class DatabaseConnector {
    * @returns Gibt die Daten zurück, als Fehler werden alle IDs auf -1 gesetzt
    */
   async registerDataminer(requestData: { dataminerName: string, sessionID: number, device: { deviceID?: number, deviceName: string, deviceType: string, firmware: string, generation: string, MACADRESS: string, sensorInformation: { sensorTypeID: number, sensorName: string, sensorUniqueID: number; }[]; }; }): Promise<{ dataminerID: number, device: { deviceID: number, sensorID: number[]; }, project: { projectID: number, projectName: string, sessionID: number; }; }> {
-    const data: string = await this.sendRequest("registerDataminer", requestData);
+    const response = await this.sendRequest("registerDataminer", requestData);
     try {
-      const result: { dataminerID: number, device: { deviceID: number, sensorID: number[]; }, project: { projectID: number, projectName: string, sessionID: number; }; } = JSON.parse(data);
+      const result: { dataminerID: number, device: { deviceID: number, sensorID: number[]; }, project: { projectID: number, projectName: string, sessionID: number; }; } = JSON.parse(response.json());
       return result;
     } catch (e) {
       console.log(e);
@@ -218,9 +218,9 @@ export class DatabaseConnector {
    * @returns Gibt die Daten zurück, als Fehler werden alle IDs auf -1 gesetzt
    */
   async registerAIModelUser(requestData: { aiModelUserName: string, modelID: number, device: { deviceID?: number, deviceName: string, deviceType: string, firmware: string, generation: string, MACADRESS: string, sensorInformation: { sensorTypeID: number, sensorName: string, sensorUniqueID: number; }[]; }; }): Promise<{ aiModelUserID: number, device: { deviceID: number, sensorID: number[]; }, project: { projectID: number, projectName: string, sessionID: -1; }; }> {
-    const data: string = await this.sendRequest("registerAIModelUser", requestData);
+    const response = await this.sendRequest("registerAIModelUser", requestData);
     try {
-      const result: { aiModelUserID: number, device: { deviceID: number, sensorID: number[]; }, project: { projectID: number, projectName: string, sessionID: -1; }; } = JSON.parse(data);
+      const result: { aiModelUserID: number, device: { deviceID: number, sensorID: number[]; }, project: { projectID: number, projectName: string, sessionID: -1; }; } = JSON.parse(response.json());
       return result;
     } catch (e) {
       console.log(e);
@@ -234,9 +234,9 @@ export class DatabaseConnector {
    * @param password 
    */
   async loginAdmin(requestData: { adminEmail: string, password: string; }): Promise<{ admin: { adminID: number, deviceID: number, adminName: string, email: string, device: { deviceID?: number, deviceName: string, deviceType: string, firmware: string, generation: string, MACADRESS: string, sensorInformation: { sensorTypeID: number, sensorName: string, sensorUniqueID: number; }[]; }; }; }> {
-    const data: string = await this.sendRequest("loginAdmin", requestData);
+    const response = await this.sendRequest("loginAdmin", requestData);
     try {
-      const result: { admin: { adminID: number, deviceID: number, adminName: string, email: string, device: { deviceID?: number, deviceName: string, deviceType: string, firmware: string, generation: string, MACADRESS: string, sensorInformation: { sensorTypeID: number, sensorName: string, sensorUniqueID: number; }[]; }; }; } = JSON.parse(data);
+      const result: { admin: { adminID: number, deviceID: number, adminName: string, email: string, device: { deviceID?: number, deviceName: string, deviceType: string, firmware: string, generation: string, MACADRESS: string, sensorInformation: { sensorTypeID: number, sensorName: string, sensorUniqueID: number; }[]; }; }; } = JSON.parse(response.json());
       return result;
     } catch (e) {
       console.log(e);
@@ -259,9 +259,9 @@ export class DatabaseConnector {
    * @returns labelID
    */
   async createLabel(requestData: { sessionID: number, userID: number, datasetID: number, label: { span: { start: number, end: number; }, labelName: string; }; }): Promise<number> {
-    const data: string = await this.sendRequest("createLabel", requestData);
+    const response = await this.sendRequest("createLabel", requestData);
     try {
-      const result: number = JSON.parse(data);
+      const result: number = JSON.parse(response.json());
       return result;
     } catch (e) {
       console.log(e);
@@ -277,9 +277,9 @@ export class DatabaseConnector {
    * @param label 
    */
   async setLabel(requestData: { sessionID: number, userID: number, datasetID: number, label: { labelID: number, span: { start?: number, end?: number; }, labelName?: string; }; }): Promise<boolean> {
-    const data: string = await this.sendRequest("setLabel", requestData);
+    const response = await this.sendRequest("setLabel", requestData);
     try {
-      const result: boolean = JSON.parse(data);
+      const result: boolean = JSON.parse(response.json());
       return result;
     } catch (e) {
       console.log(e);
@@ -295,9 +295,9 @@ export class DatabaseConnector {
    * @param labelID 
    */
   async deleteLabel(requestData: { sessionID: number, userID: number, datasetID: number, labelID: number; }): Promise<boolean> {
-    const data: string = await this.sendRequest("deleteLabel", requestData);
+    const response = await this.sendRequest("deleteLabel", requestData);
     try {
-      const result: boolean = JSON.parse(data);
+      const result: boolean = JSON.parse(response.json());
       return result;
     } catch (e) {
       console.log(e);
@@ -306,11 +306,13 @@ export class DatabaseConnector {
 
   }
 
-  private async sendRequest(action: string, requestData?: object): Promise<string> {
+  private async sendRequest(action: string, requestData?: object): Promise<any> {
     const headers = { 'Content-Type': 'application/json' };
-    const response = await fetch(DatabaseConnector.databasePHPURL + "?action=" + action, { headers, body: JSON.stringify(requestData) }); //TODO body: requestDataJSON wirft Fehler
-    const data = response.json();
-    return data;
+    try {
+      return fetch(DatabaseConnector.databasePHPURL + "?action=" + action, { headers, body: JSON.stringify(requestData) }); //TODO body: requestDataJSON wirft Fehler
+    } catch (e) {
+      return "";
+    }
   }
 }
 
