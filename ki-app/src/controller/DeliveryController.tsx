e9ae2d447452220f34b8740982b06efb68e5fdd7
Darius Seiter <Darius.Seiter@outlook.com>
2021-02-24 14:55:54 +0100

    Kommentare hinzugefügt

--- ki-app/src/controller/DeliveryController.tsx
+++ ki-app/src/controller/DeliveryController.tsx
@@ -6,53 +6,60 @@ import { DeliveryPage } from "../view/pages/DeliveryPage/index";
 //import { DeliveryFormat } from "../model/DeliveryFormat"
 
 export class DeliveryController implements PageController {
-
+    /**
+    * Seite welche gerade von dem Controller verwaltet wird
+    */
     private page: Page;
+    /**
+    * Status der Seite
+    */
     private state: IState;
 
     /**
     * Konstruktor des Seitenverwalters. Registriert sich als Beobachter auf seiner Seite und setzt den start Status.
     * @param currentProject Die momentanen Projectdaten für welches man ein Model ausliefern möchte. 
     */
-    constructor(currentProject: { projectID: number, projectName: string, choosenAIModelID: number; }) {
-        this.page = new DeliveryPage({});
-        this.page.attach(this);
-        this.state = this.page.getState()
-        this.state.currentProject = currentProject
-        this.page.setState(this.state)
+    constructor ( currentProject: { projectID: number, projectName: string, choosenAIModelID: number; } ) {
+        this.page = new DeliveryPage( {} );
+        this.page.attach( this );
+        this.state = this.page.getState();
+        this.state.currentProject = currentProject;
+        this.page.setState( this.state );
     }
 
     /**
     * Die Update Methode des Seitenverwalters.
     */
-    update() {
+    update () {
         this.state = this.page.getState();
-        switch (this.state.currentState) {
+        switch ( this.state.currentState ) {
             case States.DeliverWeb:
                 this.deliverAsWebApp();
                 break;
             case States.NeedMessage:
-                this.page.setState(MainController.getInstance().getMessage(this.state.messages));
+                this.page.setState( MainController.getInstance().getMessage( this.state.messages ) );
                 break;
             default:
                 break;
         }
     }
 
+    //TODO Die Auslieferung des Modells ist noch nicht fertig
+
     /**
     * Holt sich aus dem Status alle angegebene Email-Adressen und gibt diese mit Daten zum Projekt an die Fassade welche eine Email an alle Adressen sendet.
     * Der Inhalt der Email gibt zugriff auf das Modell zur klassifizierung.
     */
-    deliverAsWebApp() {
+    deliverAsWebApp () {
         //let emails: string[] = this.state.chosenEmails!;
-       // for (let index = 0; index < emails.length; index++) {
-            //const element: string = emails[index];
-            //let url = MainController.getInstance().getFacade().getAIModel(this.state.currentProject?.choosenAIModelID!, DeliveryFormat.WEB_APP);
-            //Maincontroller.getInstance().getFacade().sendMail(url)
-      //  }
+        // for (let index = 0; index < emails.length; index++) {
+        //const element: string = emails[index];
+        //let url = MainController.getInstance().getFacade().getAIModel(this.state.currentProject?.choosenAIModelID!, DeliveryFormat.WEB_APP);
+        //Maincontroller.getInstance().getFacade().sendMail(url)
+        //  }
     }
 
-    deliverAsExe() {
+    deliverAsExe () {
         //todo in Quality Controll
     }
 }
