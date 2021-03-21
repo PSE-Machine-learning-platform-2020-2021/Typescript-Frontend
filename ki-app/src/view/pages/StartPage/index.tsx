7f1ba52dabe282f007a8c62a9adeb3eb3b0bf01b
Darius Seiter <Darius.Seiter@outlook.com>
2021-03-21 20:07:19 +0100

    kommentiert und formatiert

--- ki-app/src/view/pages/StartPage/index.tsx
+++ ki-app/src/view/pages/StartPage/index.tsx
@@ -5,75 +5,102 @@ import { PageController } from "../../../controller/PageController";
 import { State } from "./State";
 import ReactDOM from 'react-dom';
 import { States } from '../State';
-import {NotificationContainer, NotificationManager} from 'react-notifications';
+import { NotificationContainer, NotificationManager } from 'react-notifications';
 import 'react-notifications/lib/notifications.css';
 
+
+/**
+ * Die Darstellungsseite für die Startseite der Datenerfasser
+ */
 export class StartPage implements Page {
-    state: State
+    state: State;
     observers: PageController[] = [];
 
-    constructor(admin: string) {
-        this.state = new State()
-        this.update()
-        NotificationManager.success(admin)
+    /**
+     * Konstruktor der Darstellungseite
+     * @param admin Name der Person für den aufgenommen wird
+     */
+    constructor ( admin: string ) {
+        this.state = new State();
+        this.update();
+        NotificationManager.success( admin );
     }
 
-    update() {
-        this.notify()
+    /**
+    * Update Methode der Darstellungsseite. Diese Methode wird nach jeder Änderung, die kein Seitenwechsel ist, aufgerufen. 
+    * Die Methode enthält den Aufbau der Seite und wird von ihr gerendert.
+    * Es werden durch notify() alle controller über ein Update informiert und alle Seiten Elemente werden aktualisiert und erneut gerendert. 
+    */
+    update () {
+        this.notify();
         const VDOM = (
             <div>
                 <Title />
-                <Input availableSensorTypes = {this.state.recordingSettings.availableSensorTypes} pageChangeSettings = {this.changeSettings.bind(this)}/>
-                <NotificationContainer/>
+                <Input availableSensorTypes={ this.state.recordingSettings.availableSensorTypes } pageChangeSettings={ this.changeSettings.bind( this ) } />
+                <NotificationContainer />
             </div>
-            
+
         );
-        ReactDOM.render(VDOM, document.getElementById('root'));
+        ReactDOM.render( VDOM, document.getElementById( 'root' ) );
     }
 
-     /**
-     * Prüft ob der Nutzer "Start" druckt und ändert den Zustand.
-     */
-      changeSettings(recordingSettings: {
-        newDataSetName: string, 
-        usedSensorTypes: number[], 
-        waitTime: number, 
-        readTime: number, 
-        availableSensorTypes: { sensorTypID: number, sensorType: string, chosen: boolean; }[] 
-    }) {
+    /**
+    * Prüft ob der Nutzer "Start" druckt und ändert den Zustand.
+    */
+    changeSettings ( recordingSettings: {
+        newDataSetName: string,
+        usedSensorTypes: number[],
+        waitTime: number,
+        readTime: number,
+        availableSensorTypes: { sensorTypID: number, sensorType: string, chosen: boolean; }[];
+    } ) {
         this.state.recordingSettings = recordingSettings;
         this.state.currentState = States.ChangeToDataCollection;
         this.notify();
     }
 
     /**
-     * Die Methoden für Beobachtermuster
-     * @param observer Beobachter,nähmlich Controller
-     */
-     attach(observer: PageController) {
-        this.observers.push(observer);
+    * Durch diese Methode kann sich ein Controller als Beobachter anmelden.
+    * @param oberver neuer Beobachter
+    */
+    attach ( observer: PageController ) {
+        this.observers.push( observer );
     }
 
-    detach(observer: PageController) {
-        const index = this.observers.indexOf(observer, 0);
-        if (index > -1) {
-            this.observers.splice(index, 1);
+    /**
+    * Durch diese Methode kann sich ein Controller als Beobachter abmelden.
+    * @param oberver Beobachter der zu entfernen ist
+    */
+    detach ( observer: PageController ) {
+        const index = this.observers.indexOf( observer, 0 );
+        if ( index > -1 ) {
+            this.observers.splice( index, 1 );
         }
     }
 
-    notify() {
-        for (let index = 0; index < this.observers.length; index++) {
-            const element = this.observers[index];
+    /**
+    * Durch diese Methode werden alle Beobachter über eine Änderung auf der Seite informiert.
+    */
+    notify () {
+        for ( let index = 0; index < this.observers.length; index++ ) {
+            const element = this.observers[ index ];
             element.update();
         }
     }
 
-    getState() {
+    /**
+    * Gibt den Status der Seite zurück
+    */
+    getState () {
         return this.state;
     }
 
-    setState(state: any) {
-        this.state = state
-        this.update()
+    /**
+     * Setzt einen neuen Zustand für die Seite und aktualisiert sie
+     * @param state neuer Zustand für die Seite
+     */
+    setState ( state: any ) {
+        this.state = state;
+        this.update();
     }
 }
