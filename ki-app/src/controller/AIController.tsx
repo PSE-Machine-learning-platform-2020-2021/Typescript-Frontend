8d386d525d742cc24b33528f441689643882881e
Morridin <paulza@gmx.de>
2021-04-08 15:49:27 +0200

    URL-Parameter angepasst.

--- ki-app/src/controller/AIController.tsx
+++ ki-app/src/controller/AIController.tsx
@@ -132,7 +132,7 @@ export class AIController implements PageController {
      * Klassifiziert den Datensatz.
      */
     private classifyResult () {
-        MainController.getInstance().getFacade().classify( +this.urlParams.get( "aiID" )!, this.dataSetID, this.callback );
+        MainController.getInstance().getFacade().classify( +this.urlParams.get( "modelID" )!, this.dataSetID, this.callback );
     }
 
     /**
